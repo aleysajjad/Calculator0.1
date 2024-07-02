28c31b78a0f17e5db7257c3b427e05333313287a
Sajjad Ali <157633197+aleysajjad@users.noreply.github.com>
2024-07-02 21:09:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function pick (val){
+    document.getElementById("a").value +=val ;
+}
+function solve (){
+    var x=document.getElementById("a").value;
+    var y=eval(x);
+    document.getElementById("a").value=y;
+}
